9d9e73fb23507616c4c4462fe762462066245b77
hiwad rashad <hiwadrashad112@hotmail.com>
2021-04-02 14:38:30 +0200

    name change

--- GoogleChromeRebuild/Startup.cs
+++ GoogleChromeRebuild/Startup.cs
@@ -50,7 +50,7 @@ namespace GoogleChromeRebuild
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=MainPage}/{id?}");
             });
         }
     }
